6bdf6de18e720aac8fe1d397416e2503b2f8c76c
Alexey Huba <sadchi@gmail.com>
2015-02-26 20:57:46 +0300

    initial

---
+++ project.clj
@@ -0,0 +1,43 @@
+(defproject
+  report-v2 "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2913"]
+                 [reagent "0.5.0-alpha3"]
+                 [secretary "1.2.1"]]
+
+
+  :plugins [[lein-cljsbuild "1.0.5"]
+            [lein-haml-sass "0.2.7-SNAPSHOT"]]
+
+  :haml {:src              "haml"
+         :output-directory "target"}
+
+  :sass {:src              "sass"
+         :output-directory "target"
+         :output-extension "css"}
+
+  :cljsbuild {:builds
+              {:report
+               {:source-paths ["src"]
+                :compiler     {:output-to     "target/report.js"
+                               :output-dir    "target/out"
+                               :source-map    "target/out.js.map"
+                               :optimizations :whitespace
+                               :pretty-print  true}}}}
+  :profiles {:dev
+             {:plugins      [[com.cemerick/austin "0.1.6"]]
+              :source-paths ["src"]
+              :cljsbuild    {:builds {:reporter
+                                      {:compiler
+                                       {:preamble ["reagent/react.js"]}}}}}
+             :prod
+             {:cljsbuild {:builds
+                          {:reporter
+                           {:compiler {:preamble      ["reagent/react.min.js"]
+                                       :optimizations :advanced
+                                       :pretty-print  false}}}}}}
+
+  :aliases {"bj"     ["do" ["cljsbuild" "once"]]
+            "bh"     ["do" ["haml" "once"]]
+            "bc"     ["do" ["sass" "once"]]
+            "bjprod" ["with-profile" "prod" "cljsbuild" "once"]})
---
+++ src/report_v2/main.cljs
@@ -0,0 +1,1 @@
+(ns report-v2.main)
