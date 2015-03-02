894e23e3608a1d27fc41f1109be25da469fcd80b
Alexey Huba <sadchi@gmail.com>
2015-03-02 20:34:37 +0300

    ref

---
+++ target/data.js
@@ -0,0 +1,170 @@
+var data=[
+	{
+		"category" : "Functional",
+		"path" : ["A1", "Scenario t2"],
+		"status" : {
+						"SUCCESS" : 1, 
+						"FAIL>QUARANTINE" :1
+					},
+		"runs" : [
+			{
+				"status" : "SUCCESS",
+				"fails" : [],
+				"errors" : [],
+				"meta" : [
+            		{
+            			"type" : "text",
+			        	"data" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel purus et neque elementum aliquet. Fusce in elementum turpis. Curabitur nec elementum libero. Donec sit amet arcu sit amet nisi elementum blandit a nec velit. Ut augue dui, viverra pulvinar ligula sed, blandit vulputate ante. Duis suscipit neque arcu, in consequat velit posuere vitae. Aliquam malesuada viverra risus non pharetra. Vivamus accumsan leo vel auctor placerat. Morbi interdum eget lacus at euismod. Nunc ornare semper congue. Nam nisi arcu, tempor nec nisi sed, vehicula efficitur massa. Mauris condimentum odio pulvinar pharetra faucibus. Duis hendrerit vitae augue a varius."
+			        },
+			        {
+			        	"type" : "table",
+				        "name" : "The first test table",
+				        "columns" : ["C1", "C2", "C3","C4"],
+				        "data" : [
+				            [1, 2, 3, 4, 5],
+				            [0, 1, 0, 0, 0],
+				            [0, 0, 1, 0, 0],
+				            [0, 0, 0, 1, 5]]}
+			    ],
+			    "assets" : [
+			    	{
+			    		"type" : "input",
+			    		"name" : "filename",
+			    		"value" : "1.adf"
+			    	},
+			    	{
+			    		"type" : "output",
+			    		"name" : "screenshot",
+			    		"value" : "1.jpg"
+			    	}
+
+			    ]
+			},
+						{
+				"status" : "FAIL>QUARANTINE",
+				"fails" : [],
+				"errors" : [],
+				"meta" : [
+            		{
+            			"type" : "text",
+			        	"data" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel purus et neque elementum aliquet. Fusce in elementum turpis. Curabitur nec elementum libero. Donec sit amet arcu sit amet nisi elementum blandit a nec velit. Ut augue dui, viverra pulvinar ligula sed, blandit vulputate ante. Duis suscipit neque arcu, in consequat velit posuere vitae. Aliquam malesuada viverra risus non pharetra. Vivamus accumsan leo vel auctor placerat. Morbi interdum eget lacus at euismod. Nunc ornare semper congue. Nam nisi arcu, tempor nec nisi sed, vehicula efficitur massa. Mauris condimentum odio pulvinar pharetra faucibus. Duis hendrerit vitae augue a varius."
+			        }
+			    ],
+			    "assets" : [
+			    	{
+			    		"type" : "input",
+			    		"name" : "filename",
+			    		"value" : "2.adf"
+			    	}
+
+			    ]
+			}
+
+		]
+	},
+	{
+		"category" : "Statistical",
+		"path" : ["A1", "Scenario t1"],
+		"status" : {
+						"SUCCESS" :1 , 
+						"FAIL" : 1
+					},
+		"runs" : [
+			{
+				"status" : "SUCCESS",
+				"fails" : [],
+				"errors" : [],
+				"meta" : [
+            		{
+            			"type" : "text",
+			        	"data" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel purus et neque elementum aliquet. Fusce in elementum turpis. Curabitur nec elementum libero. Donec sit amet arcu sit amet nisi elementum blandit a nec velit. Ut augue dui, viverra pulvinar ligula sed, blandit vulputate ante. Duis suscipit neque arcu, in consequat velit posuere vitae. Aliquam malesuada viverra risus non pharetra. Vivamus accumsan leo vel auctor placerat. Morbi interdum eget lacus at euismod. Nunc ornare semper congue. Nam nisi arcu, tempor nec nisi sed, vehicula efficitur massa. Mauris condimentum odio pulvinar pharetra faucibus. Duis hendrerit vitae augue a varius."
+			        },
+			        {
+			        	"type" : "table",
+				        "name" : "The first test table",
+				        "columns" : ["C1", "C2", "C3","C4"],
+				        "data" : [
+				            [1, 2, 3, 4, 5],
+				            [0, 1, 0, 0, 0],
+				            [0, 0, 1, 0, 0],
+				            [0, 0, 0, 1, 5]]}
+			    ],
+			    "assets" : [
+			    	{
+			    		"type" : "input",
+			    		"name" : "filename",
+			    		"value" : "1.adf"
+			    	},
+			    	{
+			    		"type" : "output",
+			    		"name" : "screenshot",
+			    		"value" : "1.jpg"
+			    	}
+
+			    ]
+			},
+						{
+				"status" : "FAIL",
+				"fails" : [],
+				"errors" : [],
+				"meta" : [
+            		{
+            			"type" : "text",
+			        	"data" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel purus et neque elementum aliquet. Fusce in elementum turpis. Curabitur nec elementum libero. Donec sit amet arcu sit amet nisi elementum blandit a nec velit. Ut augue dui, viverra pulvinar ligula sed, blandit vulputate ante. Duis suscipit neque arcu, in consequat velit posuere vitae. Aliquam malesuada viverra risus non pharetra. Vivamus accumsan leo vel auctor placerat. Morbi interdum eget lacus at euismod. Nunc ornare semper congue. Nam nisi arcu, tempor nec nisi sed, vehicula efficitur massa. Mauris condimentum odio pulvinar pharetra faucibus. Duis hendrerit vitae augue a varius."
+			        }
+			    ],
+			    "assets" : [
+			    	{
+			    		"type" : "input",
+			    		"name" : "filename",
+			    		"value" : "2.adf"
+			    	}
+
+			    ]
+			}
+
+		]
+	},
+	{
+		"category" : "Functional",
+		"path" : ["A1", "Scenario t3"],
+		"status" : {"SUCCESS" : 1},
+		"runs" : [
+			{
+				"status" : "SUCCESS",
+				"fails" : [],
+				"errors" : [],
+				"meta" : [
+            		{
+            			"type" : "text",
+			        	"data" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel purus et neque elementum aliquet. Fusce in elementum turpis. Curabitur nec elementum libero. Donec sit amet arcu sit amet nisi elementum blandit a nec velit. Ut augue dui, viverra pulvinar ligula sed, blandit vulputate ante. Duis suscipit neque arcu, in consequat velit posuere vitae. Aliquam malesuada viverra risus non pharetra. Vivamus accumsan leo vel auctor placerat. Morbi interdum eget lacus at euismod. Nunc ornare semper congue. Nam nisi arcu, tempor nec nisi sed, vehicula efficitur massa. Mauris condimentum odio pulvinar pharetra faucibus. Duis hendrerit vitae augue a varius."
+			        },
+			        {
+			        	"type" : "table",
+				        "name" : "The first test table",
+				        "columns" : ["C1", "C2", "C3","C4"],
+				        "data" : [
+				            [1, 2, 3, 4, 5],
+				            [0, 1, 0, 0, 0],
+				            [0, 0, 1, 0, 0],
+				            [0, 0, 0, 1, 5]]}
+			    ],
+			    "assets" : [
+			    	{
+			    		"type" : "input",
+			    		"name" : "filename",
+			    		"value" : "4.adf"
+			    	},
+			    	{
+			    		"type" : "output",
+			    		"name" : "screenshot",
+			    		"value" : "1.jpg"
+			    	}
+
+			    ]
+			}
+						
+
+			    ]
+	}
+]
