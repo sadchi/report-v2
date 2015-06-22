924331c158922dc8707c7d9ad7fa46cc11178a31
Alexey Huba <sadchi@gmail.com>
2015-06-22 17:32:30 +0300

    ref

--- lein-embedded-styles/src/leiningen/embedded_styles.clj
+++
@@ -1,6 +0,0 @@
-(ns leiningen.embedded-styles)
-
-(defn embedded-styles
-  "I don't do a lot."
-  [project & args]
-  (println "Hi!"))
---
+++ lib/jquery.nicescroll/project.clj
@@ -0,0 +1,2 @@
+(defproject js.jquery.nicescroll "3.4.0"
+  :dependencies [[js.jquery.main "1.11.2"]])
